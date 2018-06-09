8e58c19f9a3c2cb13c7784d0e8a0eaee0620fc3b
Aaron Peikert <aaron.peikert@posteo.de>
2018-06-09 12:20:19 +0200

    add a reading script

---
+++ cheat_read.R
@@ -0,0 +1,25 @@
+if(!require("pacman"))install.packages("pacman")
+pacman::p_load("tidyverse", "here")
+pacman::p_unload("pacman")
+
+movies <- read_csv(here("movies", "2000.csv"))
+
+movies <- list.files(here("movies")) %>%
+  here("movies", .) %>%
+  map(., read_csv)
+
+movies <- list.files(here("movies")) %>%
+  here("movies", .) %>%
+  map(., read_csv) %>% 
+  bind_rows()
+
+specs <- spec_csv(here("movies", "2000.csv"))
+
+list.files(here("movies")) %>%
+  here("movies", .) %>%
+  map(., read_csv, col_types = specs) %>% 
+  bind_rows()
+
+whats_here <- function(dir)here(dir, list.files(here(dir)))
+
+movies <- whats_here("movies") %>% map_dfr(read_csv, col_types = specs)
