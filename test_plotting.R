5be352a7f4b2f4b149c4dfd0fabc40e6d9fabd8d
Aniko Maraz <aniko.maraz@gmail.com>
2018-06-13 22:12:53 +0200

    this is another commit message.

--- test_plotting.R
+++ test_plotting.R
@@ -1,5 +1,13 @@
 library(ggplot2)
 hist(diamonds$carat)
-plot(diamonds$carat, diamonds$cut)
-jdsfaldaj
 
+# my first git plot
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) +
+  geom_point() +
+  geom_smooth()
+
+# nicer plot
+ggplot(diamonds, aes(x=carat, y=price, color=cut)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap( ~ cut, ncol=3)
